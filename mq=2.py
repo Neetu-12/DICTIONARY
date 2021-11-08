fd89c50a26daf7c824f9c280fae1682475e221c9
Neetu-12 <93900988+Neetu-12@users.noreply.github.com>
2021-11-08 23:31:35 +0530

    mq=2.py,mq=3.py
    mq=2.py,mq=3.py

---
+++ mq=2.py
@@ -0,0 +1,6 @@
+d={"name":"Raju", "marks":56}
+n=input("enter the no.")
+if n in d:
+    print("exist")
+else:
+    print("not exist")
---
+++ mq=3.py
@@ -0,0 +1,9 @@
+m = {
+    'data1':100,
+    'data2': -54,
+    'data3': 247
+    } 
+s=0
+for i in m:
+    s=s+m[i]
+print(s)
