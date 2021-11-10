fe65866cd41254df36e16f63bda281e111b4f705
Neetu-12 <93900988+Neetu-12@users.noreply.github.com>
2021-11-10 12:45:52 +0530

    mq=5.py
    mq=5.py

---
+++ mq=5.py
@@ -0,0 +1,31 @@
+# l1 = [1,2,3,4,5]
+# l2 = ['a','b','c','d','e']
+# d1 = {}
+# for l1_ in l1:
+#     for l2_ in l2:
+#         d1[l1_] = l2_
+#         l2.remove(l2_)
+#         break  
+
+# print (d1)
+
+###BY:-MEEEEEE.............
+# l1=["one","two","three","four","five"]
+# l2=[1,2,3,4,5,]
+# l3={}
+# for l1_ in l1:
+#     for l2_ in l2:
+#         l3[l1_]=l2_
+#         l2.remove(l2_)
+#         break
+# print(l3)
+
+l1=["one","two","three","four","five"]
+l2=[1,2,3,4,5,]
+l3={}
+for i in l1:
+    for l2_ in l2_:
+        l3[i]=l2_
+        l2.remove(l2_)
+        break
+print(l3)
