8d85f61c240a5bb9e4b8e6ccef89332629a5b3a0
Neetu-12 <93900988+Neetu-12@users.noreply.github.com>
2021-11-10 12:49:20 +0530

    qm=8.py
    qm=8.py

---
+++ qm=8.py
@@ -0,0 +1,10 @@
+# n=input("enetr the name")
+# m=int(input("enter the marks"))
+r={}
+i=1
+while i<=1:
+    n=input("enetr the name")
+    m=int(input("enter the marks"))
+    r.update({n:m})
+    i=i+1
+print(r)
