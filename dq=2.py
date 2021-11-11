24b91008bc3116034ef93af3880c50b96967c2c8
Neetu-12 <93900988+Neetu-12@users.noreply.github.com>
2021-11-11 22:18:40 +0530

    dq=2.py

---
+++ dq=2.py
@@ -0,0 +1,18 @@
+a='w3resource'
+i=0
+r={}
+while i<len(a):
+    j=0
+    c=0
+    while j<len(a):
+        if a[i]==a[j]:
+            c=c+1
+        j=j+1
+        r.update({a[i]:c})
+    i=i+1
+print(r)
+
+
+
+
+
