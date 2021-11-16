1b693fd049d861a7d835c7921cd9c22e41b0c677
Neetu <93900988+Neetu-12@users.noreply.github.com>
2021-11-16 15:42:37 +0530

    Update mq=5.py

--- mq=5.py
+++ mq=5.py
@@ -29,3 +29,15 @@ for i in l1:
         l2.remove(l2_)
         break
 print(l3)
+
+l1=["one","two","three","four","five"]
+l2=[1,2,3,4,5,]
+l3={}
+i=0
+while i<len(l1):
+    j=i
+    while j<=i:
+        r.update({l1[i]:l2[j]}
+        j=j+1
+    i=i+1
+print(r)
