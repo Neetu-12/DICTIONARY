806b7f1a7d13c6b1f15a7bed199c1177fcceb943
Neetu <93900988+Neetu-12@users.noreply.github.com>
2021-11-18 15:06:11 +0530

    Add files via upload

---
+++ dq=33.py
@@ -0,0 +1,12 @@
+a={'Aex':{'class':'V','rolld_id':2},'Puja':{'class':'V','roll_id':3}}
+for i in a:
+    print(i)
+    for j in a[i]:
+        print(j,":",a[i][j])
+#O/P:- 
+    # Aex                                                                                                           
+    # class : V                                                                                                     
+    # rolld_id : 2                                                                                                  
+    # Puja                                                                                                          
+    # class : V                                                                                                     
+    # roll_id : 3 
