bcfa3dba370fb6003165e24c490198a70427b604
Neetu <93900988+Neetu-12@users.noreply.github.com>
2021-11-19 22:27:53 +0530

    Update dq=48.py

--- dq=48.py
+++ dq=48.py
@@ -1,3 +1,4 @@
+1.)
 a={1: 'red', 2: 'green', 3: 'black', 4: 'white', 5: 'black'}
 c1={}
 for i in a:
@@ -7,3 +8,15 @@ for i in a:
         c=c+1
     c1.update({b:c})
 print(c1)
+
+
+2.)
+a={'1': 'Austin Little', '2': 'Natasha Howard', '3': 'Alfred Mullins', '4': 'Jamie Rowe'}
+r={}
+for i in a:
+    c=0
+    for j in a[i]:
+        c=c+1
+        b=a[i]
+    r.update({b:c})
+print(r)
