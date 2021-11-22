e7317d56fae16aedb0d86fda49643194e82f7742
Neetu <93900988+Neetu-12@users.noreply.github.com>
2021-11-22 16:35:55 +0530

    Update dq=46.py

--- dq=46.py
+++ dq=46.py
@@ -1,3 +1,4 @@
+1)
 a=[{'x': '10', 'y': '20', 'z': '30'}, {'p': '40', 'q': '50', 'r': '60'}]
 r={}
 for i in range(len(a)):
@@ -5,3 +6,12 @@ for i in range(len(a)):
         b=(int(a[i][j]))
         r.update({j:b})
 print(r)
+
+2)
+b=[{'x': '10.12', 'y': '20.23', 'z': '30'}, {'p': '40.00', 'q': '50.19', 'r': '60.99'}]
+r1={}
+for i in range(len(b)):
+    for j in b[i]:
+        c=float(b[i][j])
+        r1.update({j:c})
+print(r1)
